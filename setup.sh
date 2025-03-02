954f2a06f7f9523c2680836e5288133d3ccfc7f9
dannyjameswilliams <fs19144@bristol.ac.uk>
2025-03-02 17:10:34 +0000

    updated reqs

--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
 pip install --no-cache-dir -r requirements.txt
-pip install torch==2.1.0 --index-url https://download.pytorch.org/whl/cpu
-pip install torchvision==0.16.0 --index-url https://download.pytorch.org/whl/cpu
+pip install --no-cache-dir torch==2.1.0 --index-url https://download.pytorch.org/whl/cpu
+pip install --no-cache-dir torchvision==0.16.0 --index-url https://download.pytorch.org/whl/cpu
